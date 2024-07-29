1669e07b2e4b0d584d3e36f8f0300d36a6d70435
Rajadurai <k.rajadurai@gmail.com>
2024-07-29 16:46:17 +0530

    Fixed checkstyle issues

--- src/main/java/com/ezeeinfo/security/JwtGenerator.java
+++ src/main/java/com/ezeeinfo/security/JwtGenerator.java
@@ -16,28 +16,38 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * JwtGenerator class
+ * JWT token generator.
  */
 @Component
 public class JwtGenerator {
 
-    public long JWT_EXPIRATION = 70000;
+    /**
+     * token expiration time in milliseconds.
+     */
+    public static final long JWT_EXPIRATION = 70000;
+    /**
+     * secret key using HSS512.
+     */
     private Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     /**
-     *
+     * Generates the JWT token with all the required details.
      * @param authentication
      * @param tenantId
-     * @return
+     * @return generated token
      */
-    public String generateToken(Authentication authentication, final String tenantId) {
+    public String generateToken(final Authentication authentication,
+                                final String tenantId) {
         String username = authentication.getName();
         Date currentDate = new Date();
         Date expireDate = new Date(currentDate.getTime() + JWT_EXPIRATION);
 
         // Extract roles from the authentication object
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        List<String> roles = authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+        Collection<? extends GrantedAuthority> authorities
+                = authentication.getAuthorities();
+        List<String> roles = authorities.stream().map(
+                GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
 
         // Build the token with roles included in claims
         String token = Jwts
@@ -47,55 +57,60 @@ public class JwtGenerator {
                 .claim("roles", roles) // Inclusion of roles in claims
                 .setIssuedAt(new Date())
                 .setExpiration(expireDate)
-                .signWith(SignatureAlgorithm.HS512,secretKey).compact();
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();
         return token;
     }
 
     /**
-     *
+     * Gets the user name from token.
      * @param token
-     * @return
+     * @return user name
      */
 
-    public String getUsernameFromJWT(String token) {
-        Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
+    public String getUsernameFromJWT(final String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(secretKey)
+                .build().parseClaimsJws(token).getBody();
         return claims.getSubject();
     }
 
     /**
-     *
+     * validates if the token is valid or expired.
      * @param token
-     * @return
+     * @return true if valid else false
      */
 
-    public boolean validateToken(String token) {
+    public boolean validateToken(final String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(secretKey)
+                    .build().parseClaimsJws(token);
             return true;
         } catch (Exception ex) {
-            throw new AuthenticationCredentialsNotFoundException("JWT was exprired or incorrect",
+            throw new AuthenticationCredentialsNotFoundException(
+                    "JWT was exprired or incorrect",
                     ex.fillInStackTrace());
         }
     }
 
     /**
-     *
+     * Gets the roles details from JWT token.
      * @param token
-     * @return
+     * @return list of roles from the token
      */
 
-    public List<String> getRolesFromJWT(String token) {
-        Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
+    public List<String> getRolesFromJWT(final String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(secretKey)
+                .build().parseClaimsJws(token).getBody();
         return claims.get("roles", List.class);
     }
 
     /**
-     *
+     * Gets the Audiance details from token.
      * @param token
-     * @return
+     * @return Audiance object
      */
-    public Object getAudianceFromJWT(String token) {
-        Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
+    public Object getAudianceFromJWT(final String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(secretKey)
+                .build().parseClaimsJws(token).getBody();
         return claims.getAudience();
     }
 }
